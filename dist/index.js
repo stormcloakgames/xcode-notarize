37e19733663a6910a97d2d7afb340cc489a45aea
Jevon Wright <jevon@jevon.org>
2024-09-07 18:02:03 +1200

    Try fetching log regardless of notary status
    Trying to debug a successful 'Current status: Invalid' return code

--- dist/index.js
+++ dist/index.js
@@ -39668,6 +39668,56 @@ const archive = async ({productPath}) => {
 };
 
 
+const fetchlog = async ({archivePath, username, teamid, password, verbose}) => {
+    //
+    // Run notarytool to notarize this application. This only submits the
+    // application to the queue on Apple's server side. It does not
+    // actually tell us if the notarization was succesdful or not, for
+    // that we need to poll using the request UUID that is returned.
+    //
+
+    const args = [
+        "notarytool",
+        "log",
+        "--wait",
+        "--apple-id", username,
+        "--team-id", teamid,
+        "--password", password,
+        archivePath
+    ];
+
+    if (verbose === true) {
+        args.push("--verbose");
+    }
+
+    let xcrun = execa("xcrun", args, {reject: false});
+
+    xcrun.stdout.pipe(process.stdout);
+    xcrun.stderr.pipe(process.stderr);
+
+    const {exitCode, stdout, stderr} = await xcrun;
+
+    if (exitCode === undefined) {
+        // TODO Command did not run at all
+        throw Error("Unknown failure - notarytool did not run at all?");
+    }
+
+    if (exitCode !== 0) {
+        const response = JSON.parse(stdout);
+        if (verbose === true) {
+            console.log("STDERR", stderr);
+            console.log(response);
+        }
+
+        for (const productError of response["product-errors"]) {
+            core.error(`${productError.code} - ${productError.message}`);
+        }
+        return false;
+    }
+
+    return true;
+};
+
 const submit = async ({archivePath, username, teamid, password, verbose}) => {
     //
     // Run notarytool to notarize this application. This only submits the
@@ -39739,8 +39789,18 @@ const main = async () => {
             return await submit({archivePath: archivePath, ...configuration})
         });
 
+        // "Always check the notary log, even if notarization succeeds, 
+        // because it might contain warnings that you can fix prior to your next submission."
+        const log = await core.group('Fetching notarization log', async () => {
+            return await fetchlog({archivePath: archivePath, ...configuration})
+        });
+
         if (success == false) {
             core.setFailed("Notarization failed");
+            return;
+        }
+        if (log == false) {
+            core.setFailed("Notarization log failed");
             return;
         }
 
